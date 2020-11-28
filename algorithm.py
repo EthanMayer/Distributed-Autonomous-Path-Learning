7678872ed66e5acedeeee891fcbb9993ffe3e0dd
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-11-28 10:38:49 -0600

    wip

--- algorithm.py
+++ algorithm.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
             ultrasonic.pwm_S.setServoPwm('0',middleHoriz)
             ultrasonic.pwm_S.setServoPwm('1',middleVert)
             # Allow time to settle:
-            time.sleep(2)
+            time.sleep(3)
 
             while True:
                 # Grab distance from bot to object
--- debugUtils.py
+++ debugUtils.py
@@ -1,4 +1,5 @@
 import logging
+import traceback
 
 def enterREPL():
     while True:
