d3f264718447f79419bebf201a741e9fd5ef6c8f
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-11-28 09:17:23 -0600

    wip optical flow

--- algorithm.py
+++ algorithm.py
@@ -7,6 +7,7 @@ import logging
 
 from enum import Enum
 import sys
+import math
 
 # Car configurations/profiles
 class CarConfig(Enum):
@@ -36,7 +37,9 @@ sleep_time_short = 0.01 #0.1
 sleep_time_long = 0.4 #0.2
 
 # Algorithm modifiers
-stop_cond = 1 # Selects whether to stop based on distance polling(0) or with gyroscope(1).
+stop_cond = 2 # 1 # Selects whether to stop based on 
+                  # distance polling(0) [somewhat unreliable depending on obstacle course] 
+                  # or with gyroscope(1) [BROKEN], or optical flow(2).
 dist_epsilon = 30 #1   # For stop_cond == 0
 angle_epsilon = 5      # For stop_cond == 1
 
@@ -97,9 +100,13 @@ if __name__ == '__main__':
         # Normally, this would work, but we have GPIO 0 on the RPi as our gyroscope data 
         # connection.: i2c = busio.I2C(board.SCL, board.SDA)
         # Instead, we use:
-        i2c = busio.I2C(28, 27)
+        # This doesn't work, todo: i2c = busio.I2C(28, 27)
+        # sudo i2cdetect -y 1
 
         mpu = adafruit_mpu6050.MPU6050(i2c)
+    elif stop_cond == 2:
+        import flow
+        flow = flow.OpticalFlow(show_debug=True)
 
     try:
         while True:
@@ -148,10 +155,10 @@ if __name__ == '__main__':
             # (important to prevent reading a larger value again)
             time.sleep(0.1)
 
-            # Prepare gyro variables
-            if stop_cond == 1:
+            # Prepare gyro or optical flow variables
+            if stop_cond == 1 or stop_cond == 2:
                 time_total = 0
-                degrees_total = 0
+                degrees_total = 90
 
                 # ( https://stackoverflow.com/questions/1938048/high-precision-clock-in-python )
                 prev_time = time.time_ns() / 1000 / 1000 / 1000  # ns to seconds
@@ -161,15 +168,19 @@ if __name__ == '__main__':
             # that keeps distance the same regardless of sensor position, but that is unlikely):
             while True:
                 # Turn
+                dir = None
                 if destination_angle < 90:
                     print("Turning left")
                     left(None)
+                    dir = -1
                 elif destination_angle == 90:
                     print("Forward but this shouldn't happen")
                     forward()
+                    dir = 0
                 elif destination_angle > 90:
                     print("Turning right")
                     right(None)
+                    dir = 1
                 else:
                     raise Exception("Unexpected case")
 
@@ -194,7 +205,29 @@ if __name__ == '__main__':
                     degrees_total += velZ * time_diff
                     if abs(degrees_total - destination_angle) <= angle_epsilon:
                         break
+                elif stop_cond == 2:
+                    current_time = time.time_ns() / 1000 / 1000 / 1000
 
+                    # Get current distance as we turn
+                    c = ultrasonic.get_distance() 
+
+                    # Optical flow
+                    (closestPointToCenter, flowVector) = flow.computeCentermostFlow()
+                    print("Flow reliability (lower is better): " 
+                            + flow.reliabilityOfPoint(closestPointToCenter))
+                    rads = flow.computeRadiansOfCameraRotation(c, flowVector)
+                    degrees = math.degrees(rads)
+                    print("Turned " + degrees + " degrees")
+                    
+                    time_diff = current_time - prev_time
+                    prev_time = current_time
+                    degrees_total += dir * degrees * time_diff
+                    if dir == -1:
+                        if degrees_total - destination_angle <= angle_epsilon:
+                            break
+                    elif dir == 1:
+                        if destination_angle - degrees_total <= angle_epsilon:
+                            break
                 else:
                     raise Exception("Unimplemented stop_cond")
 
---
+++ flow.py
@@ -0,0 +1,173 @@
+# Based on https://www.geeksforgeeks.org/python-opencv-optical-flow-with-lucas-kanade-method/
+
+import numpy as np 
+import cv2 
+
+# Helper functions #
+
+# Function to return a pair of:
+#   1. closest "K" points to the origin, i.e. [0, 0], in a list `points` of 
+# lists where each list in `points` has 2 elements representing a 2D vector.
+# For example,
+# ```
+# points = [[3, 3], [5, -1], [-2, 4]]
+# K = 2
+# print(pClosest(points, K))
+# ```
+# will print [[3, 3], [-2, 4]] since those are the 2 closest points to [0, 0], the origin.
+#   2. indices of those items in the list.
+def closestKPointsToOrigin(points, K):
+    if len(points) == 0:
+        return None
+    if K == 1:
+        best = points[0]
+        bestIndex = 0
+        for i in range(0, len(points)):
+            p = points[i]
+            if p[0]**2 + p[1]**2 < best:
+                best = p
+                bestIndex = i
+        return (best, bestIndex)
+    else:
+        # Algorithm from https://www.geeksforgeeks.org/find-k-closest-points-to-the-origin/ ,
+        # with modifications :
+        indicesSorted = np.argsort(points) # https://numpy.org/doc/stable/reference/generated/numpy.argsort.html , https://stackoverflow.com/questions/6422700/how-to-get-indices-of-a-sorted-array-in-python
+        points.sort(key = lambda K: K[0]**2 + K[1]**2)
+        return (points[:K], indicesSorted)
+
+# Returns the angle in radians between two vectors of any dimension.
+def angleBetween(vector_1, vector_2):
+    # From https://www.kite.com/python/answers/how-to-get-the-angle-between-two-vectors-in-python :
+    unit_vector_1 = vector_1 / np.linalg.norm(vector_1)
+    unit_vector_2 = vector_2 / np.linalg.norm(vector_2)
+    dot_product = np.dot(unit_vector_1, unit_vector_2)
+    angle = np.arccos(dot_product)
+    return angle
+
+# Optical flow processing class #
+
+class OpticalFlow:
+    def __init__(self, source=0, frame_width=500, frame_height=600, show_debug=False):
+        #cap = cv2.VideoCapture('sample.mp4')
+        self.cap = cv2.VideoCapture(source)
+        # Set video capture properties
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+
+        self.source = source
+        self.frame_width = frame_width
+        self.frame_height = frame_height
+        self.show_debug = show_debug
+
+        self.__initializeFlowParams()
+    
+    # Compute the last optical flow and return the flow vector at the point
+    # that was centermost in the frame (i.e., is in-line with the distance sensor
+    # on the RPi).
+    # When show_debug was set to True, this method
+    # returns False to request exiting the program, otherwise it returns a pair consisting of:
+    #   1. The closest point as a list of two elements representing a 2D vector.
+    #   2. The flow vector detected at the closest point to the center.
+    def computeCentermostFlow(self):
+        ret, frame = self.cap.read() 
+        frame_gray = cv2.cvtColor(frame, 
+                                cv2.COLOR_BGR2GRAY) 
+    
+        # calculate optical flow 
+        # https://docs.opencv.org/3.4/dc/d6b/group__video__track.html#ga5d10ebbd59fe09c5f650289ec0ece5af
+        p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_gray, 
+                                            frame_gray, 
+                                            self.p0, None, 
+                                            **lk_params) 
+    
+        # Select good points 
+        good_new = p1[st == 1] 
+        good_old = self.p0[st == 1] 
+
+        if self.show_debug:
+            # draw the tracks 
+            #print(good_new)
+            for i, (new, old) in enumerate(zip(good_new,  
+                                            good_old)): 
+                a, b = new.ravel() 
+                c, d = old.ravel() 
+                mask = cv2.line(mask, (a, b), (c, d), 
+                                self.color[i].tolist(), 2) 
+                
+                frame = cv2.circle(frame, (a, b), 5, 
+                                self.color[i].tolist(), -1) 
+                
+            img = cv2.add(frame, mask) 
+        
+            cv2.imshow('frame', img) 
+        
+            k = cv2.waitKey(25) 
+            if k == 27: 
+                return False
+
+        # Find centermost point:
+        # Translate all points to be centered
+        # in the frame, i.e. relative to width/2, height/2 as the origin.
+        good_new_centered = map(lambda l: [l[0] - self.frame_width / 2, 
+                                           l[1] - self.frame_height / 2], good_new)
+        (centermost, centermostIndex) = pClosest(list(good_new_centered), 1)
+
+        # Updating Previous frame and points  
+        self.old_gray = frame_gray.copy() 
+        self.p0 = good_new.reshape(-1, 1, 2) 
+        return (centermost, np.subtract(centermost, good_old[centermostIndex]))
+
+    # Given a point, this returns how close it is to the center, i.e. how "reliable" it is
+    # to use in distance sensor calculations.
+    def reliabilityOfPoint(self, p):
+        return [p[0] - self.frame_width / 2, 
+                p[1] - self.frame_height / 2]
+    
+    def computeRadiansOfCameraRotation(self, sensorDistance, flowVector):
+        # Make flowVector into a 3D vector but contained within the plane of the 
+        # camera frame, and sensorDistance into a 3D vector going into the frame.
+        sensorDistVec3D = [0, 0, sensorDistance]
+        flowVector3D = [flowVector[0], flowVector[1], 0]
+
+        # Get angle in radians between the sensor vector and the sum of the two 
+        # vectors defined above:
+        angle = angleBetween(sensorDistVec3D, sensorDistVec3D + flowVector3D)
+
+        print(angle)
+
+        return angle
+
+    # Private methods #
+
+    # "Destructor": called upon garbage collection of this object.
+    def __del__(self):
+        if self.show_debug:
+            cv2.destroyAllWindows() 
+        cap.release()
+
+    def __initializeFlowParams(self):
+        # params for corner detection 
+        feature_params = dict( maxCorners = 100, 
+                            qualityLevel = 0.3, 
+                            minDistance = 7, 
+                            blockSize = 7 ) 
+        
+        # Parameters for lucas kanade optical flow 
+        lk_params = dict( winSize = (15, 15), 
+                        maxLevel = 2, 
+                        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 
+                                    10, 0.03)) 
+        
+        if self.show_debug:
+            # Create some random colors 
+            self.color = np.random.randint(0, 255, (100, 3)) 
+        
+        # Take first frame and find corners in it 
+        ret, old_frame = cap.read() 
+        self.old_gray = cv2.cvtColor(old_frame, 
+                                cv2.COLOR_BGR2GRAY) 
+        self.p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, 
+                                    **feature_params) 
+        
+        # Create a mask image for drawing purposes 
+        mask = np.zeros_like(old_frame) 
