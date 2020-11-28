d605dff7adc75ae1a5d5d3d38c95af63fb4a3e3d
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-11-28 10:19:14 -0600

    wip

--- algorithm.py
+++ algorithm.py
@@ -122,6 +122,8 @@ if __name__ == '__main__':
             # Move the ultrasonic sensor to the middle:
             ultrasonic.pwm_S.setServoPwm('0',middleHoriz)
             ultrasonic.pwm_S.setServoPwm('1',middleVert)
+            # Allow time to settle:
+            time.sleep(1)
 
             while True:
                 # Grab distance from bot to object
