58a9234cdca0d863f849fce9b9c84ffaf00eb35e
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-11-29 15:31:29 -0600

    wip

--- debugUtils.py
+++ debugUtils.py
@@ -1,16 +1,20 @@
 import logging
 import traceback
 
-# Must pass `locals` parameter as `locals()`, which
-# captures local variables used in the callee!
-def enterREPL(locals):
+# Must pass `globals` parameter as `globals()`
+# and `locals` parameter as `locals()`, which
+# captures global and local variables used in the 
+# callee's file and scope, respectively.
+def enterREPL(globals, locals):
+    if globals is None:
+        raise Exception("Need globals() to be passed to enterREPL()")
     if locals is None:
         raise Exception("Need locals() to be passed to enterREPL()")
 
     while True:
         try:
             code = input("> ")
-            exec(code, globals(), locals)
+            exec(code, globals, locals)
         except KeyboardInterrupt:
             return
         except:
