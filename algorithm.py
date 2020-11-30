d9e03b6ad630f114f75b23c99505afa177e1fa41
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-11-29 20:46:06 -0600

    wip

--- algorithm.py
+++ algorithm.py
@@ -254,7 +254,7 @@ if __name__ == '__main__':
                     rads = flow.computeRadiansOfCameraRotation(c, flowVector)
                     degrees = math.degrees(rads)
                     
-                    degrees_total += degrees
+                    degrees_total -= degrees
                     print("At " + str(degrees_total) + " degrees, want "
                             + str(destination_angle) + " degrees")
                     if dir == -1:
