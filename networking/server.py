6afc00f212de1b54eaf70685c9a7bb1c4529c63c
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-12-02 19:56:51 -0600

    wip

--- networking/server.py
+++ networking/server.py
@@ -7,6 +7,7 @@ import threading
 import commands
 import io
 import pickle
+import array
 
 # Sources for some of this code:
 # https://docs.python.org/3/howto/sockets.html
@@ -34,8 +35,32 @@ def readAsciiInt(data, ending=defaultSeparator):
 
 def readUInt8(data, ending=defaultSeparator):
     rest, length = readSeparatedData(data, ending)
-    return rest.cast('B'), rest, length # 'B' is for unsigned char which is size 1; see
+    return rest.cast('!B'), rest, length # 'B' is for unsigned char which is size 1 only if
+    # using a standard format specified which is "one of '<', '>', '!' or '='". '<' is 
+    # little endian, "!" is network byte order (big-endian also), and '>' is big endian. 
+    # See
     # https://docs.python.org/3.3/library/struct.html#format-characters for more info. 
+
+# This unpacks bytes `data` into an array of network byte order.
+# Returns an array in machine
+# byte order, from data which is possibly not machine byte order and is instead the
+# struct format specified by `fmt` for each item in the bytes.
+def unpack_array(fmt, data):
+    charactersToRemove = ['<', '>', '!', '=']
+    arrayFmt = fmt
+    for c in charactersToRemove:
+        arrayFmt = arrayFmt.replace(c, '')
+    size = struct.calcsize(arrayFmt) * len(data)
+    return array.array(arrayFmt).extend(struct.iter_unpack(fmt, data))
+
+# Packs an array into a bytes object from machine byte order (always the case for
+# an array.array in Python) into network byte order.
+def pack_array(arr):
+    #elemSize = arr.itemsize #struct.calcsize('!' + arr.typecode)
+    with io.StringIO() as output:
+        for elem in arr:
+            output.write(struct.pack('!' + arr.typecode, elem))
+        return output.getvalue()
 
 # Returns the int up until a space, then the rest of the data, 
 # and then the length of the rest of the data
@@ -58,8 +83,8 @@ class ClientThread:
         self.clientsocket_str = '%s:%d' % (address[0],address[1])
         self.allClients = allClients
 
-        # Car path that was uploaded. Currently is a list of angles.
-        self.path = []
+        # Car path that was uploaded. Currently is an array of angles encoded as bytes.
+        self.path = None
 
         # main loop: receive/send data from this client
         while True:
@@ -73,38 +98,56 @@ class ClientThread:
             if not NetworkCommand.has_value(commandNumber):
                 raise Exception("Invalid command number received: " + str(commandNumber))
             # We can now use `commandNumber` to run a command on the server.
-            self._do_stuff(dataRest[:itemLength], commandNumber)
+            self._do_stuff(dataRest[:itemLength], NetworkCommand(commandNumber))
         self.clientsocket.close()
         print('ClientThread', self.clientsocket_str, 'disconnected')
     
     # play with received data
-    def _do_stuff(self, data):
-        if data.startswith(str(commands.NetworkCommand.uploadPath)):
+    def _do_stuff(self, dataRest, command):
+        if command == NetworkCommand.uploadPath:
             # Upload to server needs to be handled.
             listLen = int.from_bytes(data[1:4], "little")
             self.path = list(data[2:2+listLen]) # Convert bytearray to a list
         else:
-            print("Error: unrecognized command")
+            print("Error: the command", command, "cannot be issued to a server")
+            return
         
-        # setPath: send to all clients except sender
+        # Receive the uploadPath
+        self.path = array.array('B')
+        self.path.extend(struct.iter_unpack('!B', dataRest)) # iter_unpack "returns an iterator
+        # which will read equally-sized chunks from the buffer until all its contents have 
+        # been consumed." ( https://docs.python.org/3/library/struct.html#struct.iter_unpack )
+        # array.extend: "Append items from iterable to the end of the array." [...] "If 
+        # iterable is not an array, it must be iterable and its elements must be the 
+        # right type to be appended to the array."
+        # ( https://docs.python.org/3/library/array.html#array.array.extend )
 
-        output = io.StringIO()
-        output1 = pickle.dumps(commands.NetworkCommand.setPath)
-        print(str(len(output1)) + " ", file=output)
-        output.write(output1)
+        # We have received uploadPath so now we send a bunch of setPath's:
+        # Send the path to all clients except the sender.
+
+        with io.StringIO() as output:
+            commandBytes = pickle.dumps(commands.NetworkCommand.setPath)
+            print(str(len(commandBytes)) + " ", file=output)
+            output.write(commandBytes)
+            
+            pathBytes = struct.pack('!s', self.path.tobytes())
+            print(str(len(pathBytes)) + " ", file=output)
+            output.write(pathBytes)
+
+            replyBytes = output.getvalue() # Bytes object containing entire buffer
         
-        output2 = pickle.dumps(self.path)
-        print(str(len(output2)) + " ", file=output)
-        output.write(output2)
-        #print('And so does this.', file=output)
-
-        reply = output.getvalue() # Bytes object containing entire buffer
         #reply = ('OK: %s' % data.decode()).encode()
         # Send to all clients except sender:
         for client in allClients:
             if client != self.clientsocket:
-                client.send(reply)
+                client.send(replyBytes)
         
+        def makeUploadPath():
+            output = io.StringIO()
+            commandBytes = pickle.dumps(commands.NetworkCommand.uploadPath)
+            output.write(commandBytes.)
+
+
 if __name__ == "__main__":
     # create an INET, STREAMing socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -118,8 +161,8 @@ if __name__ == "__main__":
 
     # close socket on shutdown
     def shutdown():
+        print('Shutting down')
         s.close()
-        print('Gracefull shutdown. Bye.')
     def _shutdown_handler(signum, frame):
         shutdown()
     # set signal handler
