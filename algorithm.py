f8dc7d6819a8d6629930ba6e10a1be658e83e928
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-12-03 12:02:57 -0600

    wip

--- algorithm.py
+++ algorithm.py
@@ -323,7 +323,7 @@ if __name__ == '__main__':
 
                     print("At " + str(degrees_total) + " degrees, want "
                             + str(destination_angle) + " degrees")
-                    time.sleep(0.1)
+                    #time.sleep(0.1)
                     if dir == -1:
                         if degrees_total - destination_angle <= angle_epsilon:
                             flow.reset()
