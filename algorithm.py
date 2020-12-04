796cc1c9276e5ad638b0f733433b69b5cf959b4a
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-12-04 14:53:51 -0600

    wip

--- algorithm.py
+++ algorithm.py
@@ -246,7 +246,7 @@ if __name__ == '__main__':
                 if functools.reduce(lambda a,b: a and b, np.isclose(distances, distances_prev, 0.5)):
                     # Speed up
                     print("Speeding up")
-                    speed += 0.007
+                    speed += 0.013 #0.007
                     forward(speed)
                     forwardSpeed = speed
             print("c <= d")
