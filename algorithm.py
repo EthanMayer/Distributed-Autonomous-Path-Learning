8b91f400ad127ab255b7bd0c6957ac372f899dce
Will Wu <william.wu@vanderbilt.edu>
2020-12-04 05:47:31 -0800

    slow

--- algorithm.py
+++ algorithm.py
@@ -118,7 +118,7 @@ try:
             time.sleep(0.5)
 
             # Move forward
-            speed = 0.25
+            speed = 0.1
             forward(speed)
             start_time = timer()
             flag = True
