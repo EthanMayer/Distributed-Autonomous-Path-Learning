79d4fbce1b96324930d31d018db928ddd286a670
sbond75 <43617712+sbond75@users.noreply.github.com>
2020-12-04 14:32:26 -0600

    wip

--- algorithm.py
+++ algorithm.py
@@ -222,7 +222,7 @@ if __name__ == '__main__':
                     for angle in range(start, end, inc):
                         ultrasonic.pwm_S.setServoPwm('0', angle)
                         c = getUltrasonicDistance()  # Grab distance from bot to object
-                        if abs(angle-middleHoriz) > 10 and c < 25:
+                        if abs(angle-middleHoriz) > 10 and c < 30:
                             print("Adjusted")
                             adj = abs(2*c) # Angles further off center are less important if they are close
                         else:
