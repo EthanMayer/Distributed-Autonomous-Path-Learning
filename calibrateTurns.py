04055107285dc2789dd4ebee98b3945c81b73311
Will Wu <william.wu@vanderbilt.edu>
2020-12-04 19:33:47 -0800

    calibration

---
+++ calibrateTurns.py
@@ -0,0 +1,24 @@
+from Motor import *
+
+import time
+
+def turn(speed=0.5):
+    wheels.setMotorModel(int(-4095 * speed), int(-4095 * speed),
+                         int(4095 * speed), int(4095 * speed))
+
+def stop():
+    wheels.setMotorModel(0, 0, 0, 0)
+
+
+wheels = Motor()
+speed = 0.25
+
+print("Enter an amount of time to turn in seconds.\n")
+while True:
+    timer = input("> ")
+    turn(speed)
+    time.sleep(timer)
+    stop()
+    turn(-speed)
+    time.sleep(timer)
+    stop()
